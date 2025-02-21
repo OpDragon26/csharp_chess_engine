289405e248dced71dd53c315dcc34dc4687860f8
OpDragon26 <25opdragon25@gmail.com>
2025-02-21 22:02:50 +0100

    todo

--- Main.cs
+++ Main.cs
@@ -6,3 +6,6 @@ Board.Board PlayingBoard = Board.Board.Constructor(Board.Presets.StartingPositio
 PlayingBoard.MakeMove(Move.Move.FromString("e2-e4-Q")); // Move.Move.Constructor(new int[] {4,1},new int[] {4,3},Empty) -> e2-e4
 
 PlayingBoard.PrintBoard(false);
+
+// implement en passant and castling
+// create and algorythm to find all legal moves
