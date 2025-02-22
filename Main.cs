adf05ab459078155c94f1dcfe2f8363f01c0b972
OpDragon26 <25opdragon25@gmail.com>
2025-02-22 14:16:43 +0100

    added MoveFinder

--- Main.cs
+++ Main.cs
@@ -1,13 +1,19 @@
 ﻿using Board;
 using Move;
+using Piece;
 using static Piece.Presets;
 
 Board.Board PlayingBoard = Board.Board.Constructor(Board.Presets.StartingPosition, new bool[] {true,true}, new bool[] {true,true}, new int[] {8,8});
 // Move.Move.Constructor(new int[] {4,1},new int[] {4,3},Empty) -> e2-e4
-PlayingBoard.MakeMove(Move.Move.FromString("e2-e5"));
-PlayingBoard.MakeMove(Move.Move.FromString("d7-d5")); 
-PlayingBoard.MakeMove(Move.Move.FromString("e5-d6")); 
-
+PlayingBoard.MakeMove(Move.Move.FromString("h8-h7"));
+Move.Move[] Moves = MoveFinder.SearchPiece(PlayingBoard, PieceType.Rook, true, Board.Presets.ConvertSquare("h7", false));
+PlayingBoard.PrintBoard(true);
+PlayingBoard.MakeMove(Moves[0]);
 PlayingBoard.PrintBoard(true);
 
 // create an algorythm to find all legal moves
+// - create an algorythm to find all moves with a piece of a certain color from a certain square on a certain board
+// -- find a way to do that with kings and castling
+// -- find a way to do that with pawns, including en passant and promotion
+// - create an algorythm to find all legal moves with all pieces of a certain color on a certain board
+// -- don't add moves where the king would be in check
---
+++ MoveFinder.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using Move;
+using Piece;
+using static Piece.Presets;
+
+namespace Board
+{
+    public static class MoveFinder
+    {
+        public static Move.Move[] Search(Board board, bool color)
+        {
+            ArrayList MoveList = new ArrayList();
+
+            Move.Move[] Moves = new Move.Move[] {}; // arraylist.ToArray()
+            return Moves;
+        }
+
+        public static Move.Move[] SearchPiece(Board board, PieceType role, bool color, int[] pos)
+        {
+            ArrayList MoveList = new ArrayList();
+
+            if (role != PieceType.Pawn) {
+                Pattern PiecePattern = Patterns.PiecePatterns[role];
+
+                if (PiecePattern.Repeat)
+                {
+                    for (int i = 0; i < PiecePattern.pattern.Length / 2; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (ValidIndex(pos[0] + PiecePattern.pattern[i,0] * (j + 1)) & ValidIndex(pos[1] + PiecePattern.pattern[i,1] * (j + 1))) 
+                            {
+                                int[] TargetSquare = new int[] {pos[0] + PiecePattern.pattern[i,0] * (j + 1), pos[1] + PiecePattern.pattern[i,1] * (j + 1)};
+                                Piece.Piece TargetPiece = board.board[TargetSquare[1],TargetSquare[0]];
+
+                                if (TargetPiece == Empty)
+                                {
+                                    MoveList.Add(Move.Move.Constructor(pos, TargetSquare, Empty));
+                                }
+                                else if (TargetPiece.Color != color)
+                                {
+                                    MoveList.Add(Move.Move.Constructor(pos, TargetSquare, Empty));
+                                    break;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < PiecePattern.pattern.Length / 2; i++)
+                    {
+                        if (ValidIndex(pos[0] + PiecePattern.pattern[i,0]) & ValidIndex(pos[1] + PiecePattern.pattern[i,1])) {
+                            int[] TargetSquare = new int[] {pos[0] + PiecePattern.pattern[i,0], pos[1] + PiecePattern.pattern[i,1]};
+                            Piece.Piece TargetPiece = board.board[TargetSquare[0],TargetSquare[1]];
+
+                            if (TargetPiece == Empty | TargetPiece.Color != color)
+                            {
+                                MoveList.Add(Move.Move.Constructor(pos, TargetSquare, Empty));
+                            }
+                        }
+                    }
+                }
+            }
+
+            Move.Move[] Moves = (Move.Move[])MoveList.ToArray(typeof(Move.Move));
+            return Moves;
+        }
+
+        public static bool ValidIndex(int index)
+        {
+            return index >= 0 & index < 8;
+        }
+    }
+
+    internal class Pattern
+    {
+        public int[,] pattern = new int[,] {};
+        public bool Repeat = true;
+
+        public static Pattern Constructor(int[,] pattern, bool repeat)
+        {
+            Pattern NewPattern = new Pattern();
+            NewPattern.pattern = pattern;
+            NewPattern.Repeat = repeat;
+            return NewPattern;
+        }
+    }
+
+    internal static class Patterns
+    {
+        internal static Dictionary<PieceType, Pattern> PiecePatterns = new Dictionary<PieceType, Pattern>{
+            {PieceType.Knight, Pattern.Constructor(
+                new int[,] {
+                    {2,1},
+                    {2,-1},
+                    {-2,1},
+                    {-2,-1},
+                    {1,2},
+                    {1,-2},
+                    {-1,2},
+                    {-1,-2},
+                },
+                false
+            )},
+            {PieceType.Rook, Pattern.Constructor(
+                new int[,] {
+                    {0,1},
+                    {0,-1},
+                    {1,0},
+                    {-1,0},
+                },
+                true
+            )},
+            {PieceType.Bishop, Pattern.Constructor(
+                new int[,] {
+                    {1,-1},
+                    {1,1},
+                    {-1,-1},
+                    {-1,1},
+                },
+                true
+            )},
+            {PieceType.Queen, Pattern.Constructor(
+                new int[,] {
+                    {0,1},
+                    {0,-1},
+                    {1,0},
+                    {-1,0},
+                    {1,-1},
+                    {1,1},
+                    {-1,-1},
+                    {-1,1},
+                },
+                true
+            )},
+            {PieceType.King, Pattern.Constructor(
+                new int[,] {
+                    {0,1},
+                    {0,-1},
+                    {1,0},
+                    {-1,0},
+                    {1,-1},
+                    {1,1},
+                    {-1,-1},
+                    {-1,1},
+                },
+                false
+            )},
+        };
+    }
+}
