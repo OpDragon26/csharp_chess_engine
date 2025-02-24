6e1d1200bbb2da608b4dc920bec669dcf9b8d993
OpDragon26 <25opdragon25@gmail.com>
2025-02-24 22:27:11 +0100

    update todo

--- obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ecfc758ba8c866151e30cbb4c0f573026ef044d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9805019fc2aad716e12f1eea9ce08019df64b75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
