b3573bb35becafe2af3be07fbd308263a5041ebe
OpDragon26 <25opdragon25@gmail.com>
2025-02-25 22:24:56 +0100

    knight weights

--- obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1ad040973e6ab77bcfd3df339b431b1ab7bdb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87f1b76a98717fb16b9aa47b8b9829cb42b5828")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
