4e02cc918a77ffcd43dde2d2de7527c8400eeafa
OpDragon26 <25opdragon25@gmail.com>
2025-02-27 17:44:07 +0100

    weight adjustments

--- obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bcd87e24f8d42a2f0daf08bca8acfcf0cd84687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdce51f8de08fa3c54138d5e8fd8e7ed86a6e7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
