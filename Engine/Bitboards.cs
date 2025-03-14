dca40be9fb89c3ed684dfa84b16fab4d641f0e46
OpDragon26 <25opdragon25@gmail.com>
2025-03-14 23:29:08 +0100

    Bitboards.cs

---
+++ Engine/Bitboards.cs
@@ -0,0 +1,23 @@
+using UnityEngine.PlayerLoop;
+
+namespace Bitboards
+{
+    public static class Bitboards
+    {
+        public static ulong[,] SquareBitboards = new ulong[8, 8];
+
+        public static void Init()
+        {
+            ulong init = 0x8000000000000000; // The first square
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    SquareBitboards[i, j] = init;
+                    init = init >> 1; // shift it to the right for the next square
+                }
+            }
+        }
+    }
+}
